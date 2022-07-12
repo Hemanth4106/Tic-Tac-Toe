3ef781654084a5a1f9a7686fd89d6fc05774dce4
Hemanth4106 <108258047+Hemanth4106@users.noreply.github.com>
2022-07-12 20:31:27 +0530

    Update tictactoe1.cpp

--- tictactoe1.cpp
+++ tictactoe1.cpp
@@ -18,7 +18,7 @@ void display_board(){
 
     //Rander Game Board LAYOUT
 
-    cout<<"PLAYER - 1 [X]t PLAYER - 2 [O]\n\n";
+    cout<<"PLAYER - 1 [X]\t PLAYER - 2 [O]\n\n";
     cout<<"\t\t     |     |     \n";
     cout<<"\t\t  "<<board[0][0]<<"  | "<<board[0][1]<<"  |  "<<board[0][2]<<"\n";
     cout<<"\t\t_____|_____|_____\n";
